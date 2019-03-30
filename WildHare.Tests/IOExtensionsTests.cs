da2e3a8ada6338290cd840664d2e23d3ebf1efdf
wcrowther <wcrowther@0f34a797-f61f-454c-90bd-cc52d0c5a539>
2019-03-30 04:49:23 +0000

    Moved StringExtensionTests to IOExtensionTests.

---
+++ WildHare.Tests/IOExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+using WildHare.Extensions;
+
+namespace WildHare.Tests
+{
+    [TestFixture]
+    public class IOExtensionsTests
+    {
+        [Test]
+        public void Test_WriteToFile_Get_Base_Directory_Alternatives()
+        {
+            string codeBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            string localPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            string location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string applicationRoot = IOExtensions.GetApplicationRoot();
+
+            Assert.AreEqual(@"file:\C:\Code\Trunk\WildHare\WildHare.Tests\bin\Debug\netcoreapp2.0", codeBase);
+            Assert.AreEqual(@"C:\Code\Trunk\WildHare\WildHare.Tests\bin\Debug\netcoreapp2.0", localPath);
+            Assert.AreEqual(@"C:\Code\Trunk\WildHare\WildHare.Tests\bin\Debug\netcoreapp2.0", location);
+            Assert.AreEqual(@"C:\Code\Trunk\WildHare\WildHare.Tests", applicationRoot);
+        }
+
+        [Test]
+        public void Test_WriteToFile_ToMapPath()
+        {
+            // Thanks to Tim Brown
+            // http://codebuckets.com/2017/10/19/getting-the-root-directory-path-for-net-core-applications/
+
+            string source1 = @"\TestFile.txt".ToMapPath();
+            string source2 = @"\Helpers\TestFile.txt".ToMapPath();
+            string source3 = @"~\Helpers\TestFile.txt".ToMapPath();
+            string source4 = @"~/SourceFiles/xmlSeedSourcePlus.xml".ToMapPath();
+
+            string fileName1 = @"C:\Code\Trunk\WildHare\WildHare.Tests\TestFile.txt";
+            string fileName2 = @"C:\Code\Trunk\WildHare\WildHare.Tests\Helpers\TestFile.txt";
+            string fileName3 = @"C:\Code\Trunk\WildHare\WildHare.Tests\Helpers\TestFile.txt";
+            string fileName4 = @"C:\Code\Trunk\WildHare\WildHare.Tests\SourceFiles\xmlSeedSourcePlus.xml";
+            string fileName4a = "C:\\Code\\Trunk\\WildHare\\WildHare.Tests\\SourceFiles\\xmlSeedSourcePlus.xml";
+
+            Assert.AreEqual(fileName1, source1);
+            Assert.AreEqual(fileName2, source2);
+            Assert.AreEqual(fileName3, source3);
+            Assert.AreEqual(fileName4, source4);
+            Assert.AreEqual(fileName4a, source4);
+        }
+    }
+}
