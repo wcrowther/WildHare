d22a4f90a01c5b7325f16b14ec7adda107f9a584
wcrowther <wcrowther22@gmail.com>
2022-09-11 10:54:28 -0400

    Added Dictionary Get<>() back for compatibility.

--- WildHare/ExpandoExtensions.cs
+++ WildHare/ExpandoExtensions.cs
@@ -81,15 +81,15 @@ namespace WildHare.Extensions
             }
         }
 
-        // <summary>Gets a value from an ExpandoObject by the specified {key}. If it does not exist, the
-        // method returns the {defaultVal}. If this is not specified, it is the default for that type.</summary>
-        // public static TVal Get<TKey, TVal>(this Dictionary<TKey, TVal> dictionary, TKey key, TVal defaultVal = default)
-        // {
-        //     if (dictionary.TryGetValue(key, out TVal val))
-        //     {
-        //         return val;
-        //     }
-        //     return defaultVal;
-        // }
+        /// <summary>Gets a value from an ExpandoObject by the specified {key}. If it does not exist, the
+        /// method returns the {defaultVal}. If this is not specified, it is the default for that type.</summary>
+        public static TVal Get<TKey, TVal>(this Dictionary<TKey, TVal> dictionary, TKey key, TVal defaultVal = default)
+        {
+            if (dictionary.TryGetValue(key, out TVal val))
+            {
+                return val;
+            }
+            return defaultVal;
+        }
     }
 }
