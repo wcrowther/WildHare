dce1fe55d00dcb8a359d6c8e551988d46311d0e0
wcrowther <wcrowther22@gmail.com>
2025-04-17 14:48:21 -0400

    Updated MetaProperty to be able to right to Name (same as 7.0.0.1)

--- WildHare/Models/MetaProperty.cs
+++ WildHare/Models/MetaProperty.cs
@@ -7,70 +7,73 @@ using System.Text;
 
 namespace WildHare
 {
-    /// <summary>Simplified PropertyInfo meta data with custom Get and Set of the instance.</summary>
-    public class MetaProperty
-    {
-        private PropertyInfo propertyInfo;
-        private object modelInstance;
+	/// <summary>Simplified PropertyInfo meta data with custom Get and Set of the instance.</summary>
+	public class MetaProperty
+	{
+		private string name;
+		private PropertyInfo propertyInfo;
+		private object modelInstance;
 
-        public MetaProperty(PropertyInfo propertyinfo)
-        {
-            propertyInfo = propertyinfo;
-        }
+		public MetaProperty(PropertyInfo propertyinfo)
+		{
+			propertyInfo = propertyinfo;
+			name = propertyInfo.Name;
+		}
 
-        public MetaProperty(PropertyInfo propertyinfo, object modelinstance)
-        {
-            propertyInfo = propertyinfo;
-            modelInstance = modelinstance;
-        }
+		public MetaProperty(PropertyInfo propertyinfo, object modelinstance)
+		{
+			propertyInfo = propertyinfo;
+			modelInstance = modelinstance;
+			name = propertyInfo.Name;
+		}
 
-        public string Name  { get => propertyInfo.Name; }
+		public string Name { get => name; set => name = value; }
 
-        public Type PropertyType { get => propertyInfo.PropertyType;  }
+		public Type PropertyType { get => propertyInfo.PropertyType; }
 
-        public bool IsKey { get => propertyInfo.IsDefined(typeof(KeyAttribute), false); }
+		public bool IsKey { get => propertyInfo.IsDefined(typeof(KeyAttribute), false); }
 
-        public bool CanWrite { get => propertyInfo.CanWrite; }
+		public bool CanWrite { get => propertyInfo.CanWrite; }
 
-        public object[] Attributes() => propertyInfo.GetCustomAttributes(true);
+		public object[] Attributes() => propertyInfo.GetCustomAttributes(true);
 
-        public T AttributeOfType<T>() => propertyInfo.GetCustomAttributes(true).OfType<T>().FirstOrDefault();
+		public T AttributeOfType<T>() => propertyInfo.GetCustomAttributes(true).OfType<T>().FirstOrDefault();
 
-        public bool Implements(string interfaceName) => PropertyType.GetInterfaces().Any(a => a.Name == interfaceName);
+		public bool Implements(string interfaceName) => PropertyType.GetInterfaces().Any(a => a.Name == interfaceName);
 
-        // Public Methods
-        public dynamic GetInstanceValue(object instance = null)
-        {
-            // ModelInstance is passed in through constructor.
-            // If instance passed in as an argument to this method, use that one
-            if (instance != null)
-            {
-                modelInstance = instance;
-            }
+		// Public Methods
+		public dynamic GetInstanceValue(object instance = null)
+		{
+			// ModelInstance is passed in through constructor.
+			// If instance passed in as an argument to this method, use that one
+			if (instance != null)
+			{
+				modelInstance = instance;
+			}
 
-            // If constructor modelInstance and method argument instance are both null throw error
-            if (modelInstance == null)
-            {
-                throw new Exception("This method requires a non-null instance in the MetaModel or MetaProperty constructor or as an argument.");
-            }
+			// If constructor modelInstance and method argument instance are both null throw error
+			if (modelInstance == null)
+			{
+				throw new Exception("This method requires a non-null instance in the MetaModel or MetaProperty constructor or as an argument.");
+			}
 
-            return propertyInfo.GetValue(modelInstance, null);
-        }
+			return propertyInfo.GetValue(modelInstance, null);
+		}
 
-        public void SetInstanceValue(object value, object instance = null)
-        {
-            if (instance != null)
-            {
-                modelInstance = instance;
-            }
-            if (modelInstance == null)
-            {
-                throw new Exception("This method requires a non-null instance in the MetaModel or MetaProperty constructor or as an argument.");
-            }
+		public void SetInstanceValue(object value, object instance = null)
+		{
+			if (instance != null)
+			{
+				modelInstance = instance;
+			}
+			if (modelInstance == null)
+			{
+				throw new Exception("This method requires a non-null instance in the MetaModel or MetaProperty constructor or as an argument.");
+			}
 
-            propertyInfo.SetValue(modelInstance, value, null);
-        }
+			propertyInfo.SetValue(modelInstance, value, null);
+		}
 
-        public override string ToString() => $"Property: {Name} ({PropertyType})";
-    }
+		public override string ToString() => $"Property: {Name} ({PropertyType})";
+	}
 }
